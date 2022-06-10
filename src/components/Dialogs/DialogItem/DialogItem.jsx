e22148ca05640127a15767fa06fcc9feff90d541
Юлия Зеликова <yulyazelikova@mail.ru>
2022-06-11 00:30:18 +0300

    add elements in dialog

--- src/components/Dialogs/DialogItem/DialogItem.jsx
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -6,6 +6,7 @@ const DialogItem = (props) => {
   let path = '/dialogs/' + props.id;
   return (
     <div className={s.dialog}>
+      <div className={s.dialogAva}></div>
       <NavLink to={path}>{props.name}</NavLink>
     </div>
   )
